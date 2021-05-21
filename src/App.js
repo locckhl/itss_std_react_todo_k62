03ce648ed2cde6e0bc9fd2e46d045677efb1ac5e
locckhl <locckhl123@gmail.com>
2021-05-21 17:31:51 +0700

    Save Avatar by Storage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './styles/main.css';
 /* コンポーネント */
 import Todo from './components/Todo';
 import Login from "./components/Login";
+import Upload from "./components/Upload";
 
 import { auth, storeUserInfo, updateUser } from "./lib/firebase";
 function App() {
@@ -28,16 +29,16 @@ function App() {
     auth.signOut();
   };
   
-  // const handleImageChanged = async downlodUrl => {
-  //   await updateUser(user, downlodUrl);
-  // }
+  const handleImageChanged = async downlodUrl => {
+    await updateUser(user, downlodUrl);
+  }
 
   const HeaderContent = () => {
     if (user) {
       return (
         <div class="navbar-end">
           <div class="navbar-item">
-            {/* <Upload userImage={user.image} onSletctedImage={handleImageChanged} /> */}
+            <Upload userImage={user.image} onSletctedImage={handleImageChanged} />
             {user.name}
           </div>
           <div class="navbar-item">
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -95,4 +95,15 @@ export const updateUser = async (user, image) => {
   }
 };
 
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+};
 export default firebase;
