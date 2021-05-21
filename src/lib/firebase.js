5a8be5a146989c5bb50e6b726dc45790cb61e61f
locckhl <locckhl123@gmail.com>
2021-05-21 15:09:05 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAbWW8J9HN46cR1Etle9VzlaKP5bcOubYg",
+  authDomain: "fir-sample-36169.firebaseapp.com",
+  projectId: "fir-sample-36169",
+  storageBucket: "fir-sample-36169.appspot.com",
+  messagingSenderId: "97199276736",
+  appId: "1:97199276736:web:1273744b663af862f2a325",
+};
+
+firebase.initializeApp(firebaseConfig);
